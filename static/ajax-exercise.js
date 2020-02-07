df2379f1c0774204e088aeea247f957131ba4e80
Your Name <you@example.com>
2020-02-07 13:06:23 -0800

    Added functions to get fortune, get weather and order melons

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -6,12 +6,12 @@
 function showFortune(evt) {
 
     // TODO: get the fortune and show it in the #fortune-text div
-}
+    $.get('/fortune', (res) => {
+      $('#fortune-text').html(res);
+    });
+};
 
 $('#get-fortune-button').on('click', showFortune);
-
-
-
 
 
 // PART 2: SHOW WEATHER
@@ -24,7 +24,10 @@ function showWeather(evt) {
 
 
     // TODO: request weather with that URL and show the forecast in #weather-info
-}
+    $.get(url, formData, (res) => {
+      alert(`Your weather forecast is ${res.forecast}`);
+    });
+};
 
 $("#weather-form").on('submit', showWeather);
 
@@ -36,9 +39,15 @@ $("#weather-form").on('submit', showWeather);
 function orderMelons(evt) {
     evt.preventDefault();
 
+    const formData = {melon_type: $('#melon-type-field').val(),
+                      qty: $('#qty-field').val()};
+
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
-}
+    $.post("/order-melons.json", formData, (res) =>{
+      $('#order-status').html(res.msg);
+    });
+};
 
 $("#order-form").on('submit', orderMelons);
 
